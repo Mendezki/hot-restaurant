9b3714203467ae9457f1b8f176b7fcfc6ddc2a70
cnpham <44155537+cnpham@users.noreply.github.com>
2019-02-02 11:05:14 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ var path = require("path");
 
 var app = express();
 var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+    console.log("Server is listening on PORT: " + PORT);
+})
