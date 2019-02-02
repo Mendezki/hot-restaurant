7db066588ddeda1bf0175beeaebc5104e2272f6b
cnpham <44155537+cnpham@users.noreply.github.com>
2019-02-02 11:42:26 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 8080;
+var PORT = 3000;
+
+
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(___dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(___dirname, "tables.html"));
+});
+
+app.get("/reservations", function(req, res) {
+    res.sendFile(path.join(___dirname, "reservations.html"));
+});
 
 var server = http.createServer(handleRequest);
+function handleRequest(req, res) {
+    var path = req.url;
+} 
 
 server.listen(PORT, function() {
     console.log("Server is listening on PORT: " + PORT);
